7a4f9f9b6852921b743c4230214a194f4d1c2a9d
Leonid Beder <leonid@jammer.fm>
2015-05-12 16:51:24 +0300

    Restored test script.

---
+++ test/test_script.rb
@@ -0,0 +1,21 @@
+require 'eyes_selenium'
+require 'logger'
+
+eyes = Applitools::Eyes.new
+
+my_webdriver = Selenium::WebDriver.for :chrome
+eyes.api_key = ENV['APPLITOOLS_API_KEY']
+eyes.log_handler = Logger.new(STDOUT)
+
+begin
+  eyes.test(app_name: 'Ruby SDK', test_name: 'Applitools website test', viewport_size: {width: 1024, height: 768},
+    driver: my_webdriver) do |driver|
+    driver.get 'http://www.applitools.com'
+    eyes.check_window('initial')
+    eyes.check_region(:css, '.pricing', 'Pricing button')
+    driver.find_element(:css, '.pricing a').click
+    eyes.check_window('pricing page')
+  end
+ensure
+  my_webdriver.quit
+end
