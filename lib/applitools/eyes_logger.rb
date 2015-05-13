2ccba4bf31027d2f94fff4c99edf1d3b51c51e0a
Leonid Beder <leonid@jammer.fm>
2015-05-13 13:25:41 +0300

    Relaxed the required interface.

--- lib/applitools/eyes_logger.rb
+++ lib/applitools/eyes_logger.rb
@@ -13,13 +13,28 @@ module Applitools::EyesLogger
   extend Forwardable
   extend self
 
-  def_delegators :@@log_handler, :debug, :info, :warn, :error, :fatal, :open, :close
+  MANDATORY_METHODS = [:debug, :info, :close].freeze
+  OPTIONAL_METHODS = [:warn, :error, :fatal].freeze
+
+  def_delegators :@@log_handler, *MANDATORY_METHODS
 
   @@log_handler = NullLogger.new
 
   def log_handler=(log_handler)
-    raise Applitools::EyesError.new('log_handler must implement Logger!') unless log_handler.is_a?(Logger)
+    raise Applitools::EyesError.new('log_handler must implement Logger!') unless valid?(log_handler)
 
     @@log_handler = log_handler
+  end
+
+  OPTIONAL_METHODS.each do |method|
+    define_singleton_method(method) do |msg|
+      @@log_handler.respond_to?(method) ? @@log_handler.send(method, msg) : @@log_handler.info(msg)
+    end
+  end
+
+  private
+
+  def valid?(log_handler)
+    MANDATORY_METHODS.all? {|method| log_handler.respond_to?(method)}
   end
 end
