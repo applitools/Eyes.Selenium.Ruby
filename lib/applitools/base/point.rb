b31be200a63c04af4d2389b97b89f3a4ac16aab5
Leonid Beder <leonid.beder@applitools.com>
2015-05-26 17:44:01 +0300

    Point should be comparable to itself.

--- lib/applitools/base/point.rb
+++ lib/applitools/base/point.rb
@@ -5,12 +5,23 @@ module Applitools::Base
     alias_attribute :left, :x
     alias_attribute :top, :y
 
-    TOP_LEFT = Point.new(0, 0)
-
     def initialize(x, y)
       @x = x
       @y = y
     end
+
+    TOP_LEFT = Point.new(0, 0)
+
+    def ==(other)
+      return super.==(other) unless other.is_a?(Point)
+      @x == other.x && @y == other.y
+    end
+
+    def hash
+      @x.hash & @y.hash
+    end
+
+    alias_method :eql?, :==
 
     def to_hash(options = {})
       options[:region] ? { left: left, top: top } : { x: x, y: y }
