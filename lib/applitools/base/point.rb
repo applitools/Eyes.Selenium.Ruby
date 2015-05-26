8dc88b35e54833327037aeead282023a34a3f2cc
Leonid Beder <leonid.beder@applitools.com>
2015-05-26 13:27:32 +0300

    Added :left and :top attributes to the Point class.

--- lib/applitools/base/point.rb
+++ lib/applitools/base/point.rb
@@ -2,16 +2,16 @@ module Applitools::Base
   class Point
     attr_accessor :x, :y
 
+    alias_attribute :left, :x
+    alias_attribute :top, :y
+
     def initialize(x, y)
       @x = x
       @y = y
     end
 
-    def to_hash
-      {
-        x: x,
-        y: y
-      }
+    def to_hash(options = {})
+      options[:region] ? { left: left, top: top } : { x: x, y: y }
     end
 
     def values
