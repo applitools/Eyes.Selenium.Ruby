13fdcf5c164c43967a45c9e31948ba74a18fdfbd
Leonid Beder <leonid.beder@applitools.com>
2015-05-26 13:08:41 +0300

    Added extensions.

---
+++ lib/applitools/extensions.rb
@@ -0,0 +1,17 @@
+class Module
+  def alias_attribute(new_name, old_name)
+    module_eval <<-STR, __FILE__, __LINE__ + 1
+      def #{new_name}
+        self.#{old_name}
+      end
+
+      def #{new_name}?
+        self.#{old_name}?
+      end
+
+      def #{new_name}=(v)
+        self.#{old_name}
+      end
+    STR
+  end
+end
