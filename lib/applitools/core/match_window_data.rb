911aecd9a96b92206afdc3d7c5dee1529fdc9770
Nikita Andreyev <nikita.andreyev@easternpeak.com>
2016-08-01 13:44:02 +0300

    Applitools::Core::EyesBase class

---
+++ lib/applitools/core/match_window_data.rb
@@ -0,0 +1,7 @@
+module Applitools::Core
+  class MatchWindowData
+    attr_accessor :app_output, :user_inputs, :tag, :options, :ignore_mistmatch
+
+
+  end
+end
---
+++ spec/match_window_data_spec.rb
@@ -0,0 +1,11 @@
+require 'spec_helper'
+
+RSpec.describe Applitools::Core::MatchWindowData do
+  it_should_behave_like "responds to method", [
+      :app_output, :app_output=,
+      :user_inputs, :user_inputs=,
+      :tag, :tag=,
+      :options, :options=,
+      :ignore_mistmatch, :ignore_mistmatch=
+  ]
+end
