07931c65cccf60dfa18c4cad3cf54b9e873301b0
Nikita Andreyev <nsa31415@gmail.com>
2017-01-18 18:43:28 +0200

    performance optimization

--- lib/applitools/chunky_png/resampling.rb
+++ lib/applitools/chunky_png/resampling.rb
@@ -16,7 +16,7 @@ module Applitools::ChunkyPNG
       pixels = Array.new(points.size)
 
       points.each do |interpolation_data|
-        pixels[interpolation_data[:index]] = interpolate_cubic(interpolation_data)
+        pixels[interpolation_data[0]] = interpolate_cubic(interpolation_data)
       end
       replace_canvas!(dst_width2, height, pixels)
 
@@ -24,7 +24,7 @@ module Applitools::ChunkyPNG
       pixels = Array.new(points.size)
 
       points.each do |interpolation_data|
-        pixels[interpolation_data[:index]] = interpolate_cubic(interpolation_data)
+        pixels[interpolation_data[0]] = interpolate_cubic(interpolation_data)
       end
       replace_canvas!(dst_width2,dst_height2, pixels)
 
@@ -52,27 +52,45 @@ module Applitools::ChunkyPNG
       bicubic_points(height, dst_height, true)
     end
 
-    def bicubic_points(src_dimension, dst_dimension, direction)
+    def bicubic_points(src_dimension, dst_dimension, direction, y_start_position = 0)
       step = (src_dimension-1).to_f / dst_dimension
       y_bounds = direction ? width : height
-      pixels_size = y_bounds*dst_dimension
+      raise ArgumentError.new "Start position value is invalid!" unless y_start_position < y_bounds
+      pixels_size = (y_bounds - y_start_position)*dst_dimension
+
+      steps = Array.new(dst_dimension)
+      residues = Array.new(dst_dimension)
+
+      for i in 0..dst_dimension-1
+        steps[i] = (i*step).to_i
+        residues[i] = i*step - steps[i]
+      end
       Enumerator.new(pixels_size) do |enum|
-        for y in 0..y_bounds-1
-          line = direction ? column(y) : row(y)
+        for y in y_start_position..y_bounds-1
+          line = (direction ? column(y) : row(y))
+
+          line_with_bounds = [imaginable_point(line[0], line[1])] + line + [
+              imaginable_point(line[src_dimension-2], line[src_dimension-3]),
+              imaginable_point(line[src_dimension-1], line[src_dimension-2])
+          ]
+
           index_y = dst_dimension*y
           for x in 0..dst_dimension-1
-            pos = (x*step).to_i
-            t = x*step - pos
-            x0 = pos > 0 ? line[pos-1] : imaginable_point(line[pos], line[pos+1])
-            x1 = line[pos]
-            x2 = line[pos+1]
-            x3 = pos < src_dimension - 2 ? line[pos+2] : imaginable_point(line[pos+1], line[pos])
+            # pos = (x*step).to_i
+            # t = x*step - pos
+
+            # x0 = pos > 0 ? line[pos-1] : imaginable_point(line[pos], line[pos+1])
+            # x1 = line[pos]
+            # x2 = line[pos+1]
+            # x3 = pos < src_dimension - 2 ? line[pos+2] : imaginable_point(line[pos+1], line[pos])
+
             index = direction ? y_bounds*x + y : index_y + x
-            enum << INTERPOLATION_DATA.new(index,x0,x1,x2,x3,t)
+            enum << ([index,residues[x]] + line_with_bounds.last(src_dimension + 3 - steps[x]).first(4))
           end
         end
       end
     end
+
 
     def scale_points(dst_width, dst_height, w_m, h_m)
       Enumerator.new(dst_width*dst_height) do |enum|
@@ -115,12 +133,12 @@ module Applitools::ChunkyPNG
 
     def interpolate_cubic(data)
       result = {}
-      t = data[:t]
+      t = data[1]
       [:r, :g, :b, :a].each do |chan|
-        c0 = ChunkyPNG::Color.send(chan, data[:x0])
-        c1 = ChunkyPNG::Color.send(chan, data[:x1])
-        c2 = ChunkyPNG::Color.send(chan, data[:x2])
-        c3 = ChunkyPNG::Color.send(chan, data[:x3])
+        c0 = ChunkyPNG::Color.send(chan, data[2])
+        c1 = ChunkyPNG::Color.send(chan, data[3])
+        c2 = ChunkyPNG::Color.send(chan, data[4])
+        c3 = ChunkyPNG::Color.send(chan, data[5])
 
         a = -0.5*c0 + 1.5*c1 - 1.5*c2 + 0.5*c3
         b = c0 - 2.5*c1 + 2*c2 - 0.5*c3
@@ -139,5 +157,6 @@ module Applitools::ChunkyPNG
       a = [0, [255, ChunkyPNG::Color.a(point1) << 1].min - ChunkyPNG::Color.a(point2)].max
       ChunkyPNG::Color.rgba(r, g, b, a)
     end
+
   end
 end
